25413b4962126c2102a3ac8f7b6ea30d14900a3a
Khaled El Hamzi <contact@elhamzi.me>
2025-03-23 15:44:07 +0100

    feat(animations): add animations support and improve TodosComponent
    - Installed `web-animations-js` and imported it in `polyfills.ts`
    - Added `BrowserAnimationsModule` to `AppModule`
    - Updated `appConfig` to include `provideAnimations()`
    - Improved `todos.component.html` by adding conditional rendering and a fade-in animation
    - Enhanced `TodosComponent` with Angular animations for a smooth fade-in effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoService } from './services/todo.service';
@@ -9,6 +10,7 @@ import { TodosComponent } from './todos/todos.component'; // Import the TodosCom
   declarations: [],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AppComponent,
     TodosComponent
